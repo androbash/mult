b3eedea14a4926a93702b250c2ac888697085b6f
androbash <romanchandrew@gmail.com>
2024-10-04 16:26:43 +0300

    first commit

---
+++ lang.js
@@ -0,0 +1,32 @@
+const langArr = {
+    title: {
+        ua: 'Мульти мовна сторінка',
+        en: 'Multilingual  page',
+        de: 'Mehrsprachige Seite',
+    },
+    name: {
+        ua: 'Ноутбук Lenovo',
+        en: 'LapTop Lenovo',
+        de: 'Lenovo Notebook',
+    },
+    os: {
+        ua: 'Операційна система',
+        en: 'Operation system',
+        de: 'Betriebssystem',
+    },
+    memory: {
+        ua: "Об'єм пам'яті",
+        en: 'Memory capacity',
+        de: 'Speicherkapazität',
+    },
+    type: {
+        ua: "Тип пам'яті",
+        en: "Memory type",
+        de: 'Speichertyp',
+    },
+    color: {
+        ua: "Колір",
+        en: "Color",
+        de: 'Farbe',
+    },
+}
